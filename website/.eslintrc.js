1fcd670a4a69e232a1cd3277215d4609e40dc2da
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-04-17 23:22:03 -0600

    Final set up of framework.

---
+++ website/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es6": true,
+      "node": true
+  },
+  "extends": [
+      "plugin:react/recommended",
+      "airbnb"
+  ],
+  "globals": {
+      "Atomics": "readonly",
+      "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+    "no-plusplus": "off",
+    "arrow-body-style": "off",
+    "react/forbid-prop-types": "off",
+    "no-console": "off",
+    "comma-dangle": "off"
+  }
+};
--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 
 const App = () => {
-
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       Hello World?
+      {count}
+      <button type="button" onClick={() => setCount(count + 1)}> LOL </button>
     </div>
-  )
-}
+  );
+};
 
 export default App;

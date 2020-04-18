f8916cb2ad7814383f079f203fbf6c9c785186c2
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-04-17 23:06:38 -0600

    Setting up ESLint and more skeleton.

--- website/src/index.jsx
+++ website/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App.jsx';
 
 ReactDOM.render(
   <h1>Hello There!</h1>,
-  document.getElementById('problemTracker'))
-;
+  document.getElementById('problemTracker')
+);

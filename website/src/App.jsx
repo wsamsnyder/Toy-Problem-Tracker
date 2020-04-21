1ff93826e63faa2419aab72d3fe81a7006e5fa62
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-04-20 23:17:35 -0600

    Setting up framework for ReactRouter.

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
-const App = () => {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      Hello World?
+      Hello World? does this work?
       {count}
       <button type="button" onClick={() => setCount(count + 1)}> LOL </button>
     </div>
   );
-};
-
-export default App;
+}
